148b40953bf57481ed90b61a58f1ff08fd8fd017
Dawid Kluczewski <notedeagle@gmail.com>
2022-01-03 15:13:59 +0100

    Added LoginController and RegistrationController

--- src/main/java/com/fuel/tracker/fueltracker/controller/RegistrationController.java
+++ src/main/java/com/fuel/tracker/fueltracker/controller/RegistrationController.java
@@ -1,7 +1,10 @@
 package com.fuel.tracker.fueltracker.controller;
 
+import com.fuel.tracker.fueltracker.config.security.RegistrationCredentials;
 import com.fuel.tracker.fueltracker.service.RegistrationService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class RegistrationController {
 
     private final RegistrationService registrationService;
+
+    @PostMapping("/register")
+    public void register(@RequestBody RegistrationCredentials request) {
+        registrationService.register(request);
+    }
 }
