31ce3f3a20acac668388a331569e779ae2c2080d
Dawid Kluczewski <notedeagle@gmail.com>
2022-06-07 21:40:04 +0200

    N+1 fix

--- src/main/java/com/fuel/tracker/fueltracker/repository/VehicleRepository.java
+++ src/main/java/com/fuel/tracker/fueltracker/repository/VehicleRepository.java
@@ -2,6 +2,7 @@ package com.fuel.tracker.fueltracker.repository;
 
 import com.fuel.tracker.fueltracker.model.entity.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +14,8 @@ public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
     Optional<List<Vehicle>> findAllByCustomerId(Long id);
 
     Optional<Vehicle> findVehicleByNameAndCustomerId(@Param("name") String name, @Param("id") Long id);
+
+    @Override
+    @Query("from Vehicle v left join fetch v.refuel left join fetch v.expense")
+    List<Vehicle> findAll();
 }
